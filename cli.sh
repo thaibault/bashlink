5816ef067eb36038a57191f2e56c784d1d53ea85
torben <t.sickert[~at~]gmail.com>
2018-03-12 12:56:59 +0100

    Automatic update to new version.

--- cli.sh
+++ cli.sh
@@ -57,22 +57,25 @@ bl_cli_color_white=''
 bl_cli_color_yellow=''
 ## endregion
 ## region unicode glyphs
+# NOTE: Each fall-back symbol should only consist of one character. To allow
+# interactive shell integration (with fixed number of printed characters to
+# replace).
 bl_cli_powerline_arrow_down='_'
 bl_cli_powerline_arrow_left='<'
 bl_cli_powerline_arrow_right='>'
 bl_cli_powerline_arrow_right_down='>'
-bl_cli_powerline_branch='|}'
-bl_cli_powerline_cog='{*}'
+bl_cli_powerline_branch='}'
+bl_cli_powerline_cog='*'
 bl_cli_powerline_fail='x'
-bl_cli_powerline_heart='<3'
+bl_cli_powerline_heart='3'
 bl_cli_powerline_lightning='!'
 bl_cli_powerline_ok='+'
 bl_cli_powerline_pointingarrow='~'
-bl_cli_powerline_plusminus='+-'
+bl_cli_powerline_plusminus='x'
 bl_cli_powerline_refersto='*'
 bl_cli_powerline_star='*'
-bl_cli_powerline_saxophone='(yeah)'
-bl_cli_powerline_thumbsup='(ok)'
+bl_cli_powerline_saxophone='y'
+bl_cli_powerline_thumbsup='+'
 ## endregion
 # NOTE: Use 'xfd -fa <font-name>' to watch glyphs.
 bl_cli_unicode_enabled=false
@@ -235,7 +238,10 @@ bl_cli_disable_unicode_glyphs() {
         saxophone \
         thumbsup
     do
-        eval "bl_cli_powerline_${name}=\"\$bl_cli_powerline_${name}_backup\""
+        if [[ "$(eval "echo \"\$bl_cli_powerline_${name}_backup\"")" != '' ]]; then
+            eval \
+                "bl_cli_powerline_${name}=\"\$bl_cli_powerline_${name}_backup\""
+        fi
     done
 }
 alias bl.cli.enable_unicode_glyphs=bl_cli_enable_unicode_glyphs
@@ -301,9 +307,9 @@ else
     bl_cli_disable_color
 fi
 # TODO this breaks dracut (segfault)
-#(bl.module.log_plain -e $'\u1F3B7' | command grep -v F3B7) &> /dev/null
+#(echo -e $'\u1F3B7' | command grep -v F3B7) &> /dev/null
 # NOTE: "bl.tools.is_defined" results in an dependency cycle.
-if bl.module.is_defined NO_UNICODE; then
+if bl.module.is_defined NO_UNICODE || ! bl.cli.glyph_available_in_font; then
     bl.cli.disable_unicode_glyphs
 else
     bl.cli.enable_unicode_glyphs
