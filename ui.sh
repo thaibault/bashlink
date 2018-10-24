6c66f342492dd37d7af7d38053294fe41647c295
torben <t.sickert[~at~]gmail.com>
2018-10-24 10:28:20 +0200

    Automatic update to new version.

--- ui.sh
+++ ui.sh
@@ -55,7 +55,44 @@ bl_ui_dialog() {
     rm --force "$output_buffer_file_path"
     return $return_code
 }
-alias bl.ui.input_password='bl.ui.dialog --clear --insecure --title Password --passwordbox "" 0 0'
+alias bl.ui.input_password=bl_ui_input_password
+bl_ui_input_password() {
+    # shellcheck disable=SC1004
+    local -r __documentation__='
+        Shows a password input dialog box. Forwards all given parameter to
+        dialog.
+
+        ```
+            bl.ui.input_password >/plain_password.txt
+        ```
+
+        ```
+            bl.ui.input_password "Give me a password" >/plain_password.txt
+        ```
+    '
+    local title=Password
+    if [[ "$1" != '' ]]; then
+        title="$1"
+    fi
+    bl.ui.dialog --clear --insecure --title "$title" --passwordbox '' 0 0
+}
+alias bl.ui.input_text='bl.ui.dialog --clear --inputbox RZ-Login 0 0'
+alias bl.ui.input_text=bl_ui_input_text
+bl_ui_input_text() {
+    # shellcheck disable=SC1004
+    local -r __documentation__='
+        Shows a text input dialog box. Forwards all given parameter to dialog.
+
+        ```
+            bl.ui.input_text "Give me a your name" >/input.txt
+        ```
+    '
+    local title=Login
+    if [[ "$1" != '' ]]; then
+        title="$1"
+    fi
+    bl.ui.dialog --clear --inputbox "$title" 0 0
+}
 # endregion
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
